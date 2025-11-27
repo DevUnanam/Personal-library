db131b30eea1d61ebd2dad1bb292970f1857d694
Smashlord564 <smashking753@gmail.com>
2025-11-27 13:32:12 +0100

    imported os

--- pinknote/config/settings.py
+++ pinknote/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t34nr1ht3qk46_j-#k@!ql9gaut3xca5aakkvvp+=665%1gl=b'
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['personal-library-4-jqbz.onrender.com', 'localhost', '127.0.0.1']
-
-
 
 # Application definition
 
